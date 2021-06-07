412af2a43835ce25829ba389cec4372dde52aaf4
tsunga59 <tatsumochi15@gmail.com>
2021-06-07 15:51:00 +0900

    ユーザー登録・ログイン画面を作成

---
+++ laravel/resources/views/auth/login.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('title', 'ログイン')
+
+@section('content')
+
+<section class="login">
+    <div class="container">
+        <form action="{{ url('/login') }}" method="post">
+            @csrf
+            <!-- エラーメッセージ -->
+            @if($errors->any())
+                <div class="error_area">
+                    @foreach($errors->all() as $error)
+                        <p>{{ $error }}</p>
+                    @endforeach
+                </div>
+            @endif
+            <h2>ログインフォーム</h2>
+            <table>
+                <tr>
+                    <th>
+                        <label for="email"><i class="fas fa-envelope"></i></label>
+                    </th>
+                    <td>
+                        <input type="email" name="email" id="email" placeholder="メールアドレス" autofocus value="{{ old('email') }}">
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <label for="password"><i class="fas fa-lock"></i></label>
+                    </th>
+                    <td>
+                        <input type="password" name="password" id="password" placeholder="パスワード">
+                    </td>
+                </tr>
+            </table>
+            <button type="submit" class="green">メールアドレスでログイン</button>
+            <p class="add">or</p>
+            <button class="red">Googleでログイン</button>
+            <button class="gray">ゲストでログイン</button>
+            <a href="{{ route('register') }}">ユーザー登録はこちら</a>
+        </form>
+    </div>
+</section>
+@endsection
