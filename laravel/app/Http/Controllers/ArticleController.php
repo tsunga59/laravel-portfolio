b3cc855b05fa331f727a3d93348129ae0f50d244
tsunga59 <tatsumochi15@gmail.com>
2021-06-09 16:34:57 +0900

    ルーティング・コントローラー・モデル・テーブルを作成

---
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * 投稿を一覧表示
+     * 
+     * @return view
+     */
+    public function index()
+    {
+        $articles = Article::all()->sortByDesc('created_at');
+
+        return view('articles.index', ['articles' => $articles]);
+    }
+
+    /**
+     * 投稿の新規作成画面を表示
+     * 
+     * @return view
+     */
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    /**
+     * 投稿の新規作成処理
+     * 
+     * @param ArticleRequest $request, Article $article
+     * @return RedirectResponse
+     */
+    public function store(ArticleRequest $request, Article $article)
+    {
+
+    }
+
+    /**
+     * 投稿の詳細を表示
+     * 
+     * @param Article $article
+     * @return view
+     */
+    public function show(Article $article)
+    {
+
+    }
+
+    /**
+     * 投稿の更新画面を表示
+     * 
+     * @param Article $article
+     * @return view
+     */
+    public function edit(Article $article)
+    {
+
+    }
+
+    /**
+     * 投稿の更新処理
+     * 
+     * @param ArticleRequest $request, Article $article
+     * @return RedirectResponse
+     */
+    public function update(ArticleRequest $request, Article $article)
+    {
+
+    }
+
+    /**
+     * 投稿の削除処理
+     * 
+     * @param Article $article
+     * @return RedirectResponse
+     */
+    public function destroy(Article $article)
+    {
+        
+    }
+}
--- laravel/resources/views/articles/index.blade.php
+++ laravel/resources/views/articles/index.blade.php
@@ -1,3 +1,4 @@
+@auth
 {{ Auth::user()->name }}
 <br>
 {{ Auth::user()->email }}
@@ -7,3 +8,7 @@
     @csrf
     <button type="submit">logout</button>
 </form>
+@else
+<a href="{{ route('register') }}">ユーザー登録</a>
+<a href="{{ route('login') }}">ログイン</a>
+@endauth
