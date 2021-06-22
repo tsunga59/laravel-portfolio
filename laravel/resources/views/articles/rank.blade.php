af7902dd251677bfe236dd7ad7194822d864938a
tsunga59 <tatsumochi15@gmail.com>
2021-06-22 15:50:02 +0900

    アプリ名・ロゴ・ファビコンを追加

--- laravel/resources/views/articles/rank.blade.php
+++ laravel/resources/views/articles/rank.blade.php
@@ -1,5 +1,4 @@
 <div class="rank_list">
-    {{-- <p>前月の朝活達成率に応じて、該当ランクのバッジが表示されます</p>
-    <br> --}}
+    {{-- <p>前月の朝活達成率に応じて、該当ランクのバッジが表示されます</p> --}}
     <p style="text-align:center; color:#646E72;">Coming soon...</p>
 </div>
